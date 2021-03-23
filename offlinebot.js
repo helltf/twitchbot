9a8dfa32bc41f27191fbb36f2bd19d92a40e695c
helltf <74050664+helltf@users.noreply.github.com>
2021-03-23 18:56:22 +0100

    added rpsgamehandler

--- offlinebot.js
+++ offlinebot.js
@@ -1,14 +1,15 @@
 const database = require('./database');
 database.connect();
-const client = require('./lib/client.js');
-const watchclient = require("./lib/watchclient");
 require('dotenv').config();
 const got = require('got')
-var mysql = require('mysql');
 const updatecmd = require("./lib/functions/updateCommandsDatabase")
+require("./lib/functions/rpsgamehandler")
+require("./lib/functions/runningrpsgames")
 updatecmd();
 
 var emoteJSON={}
 
-let runningRpsGames=[]
-module.exports.runningRpsGames=runningRpsGames;
+
+const client = require('./lib/client.js');
+const watchclient = require("./lib/watchclient");
+
