2778e1fb60430d05f51f2733b52155fab6b2d70e
helltf <74050664+helltf@users.noreply.github.com>
2021-03-24 13:46:25 +0100

    added new emotegame

--- offlinebot.js
+++ offlinebot.js
@@ -5,8 +5,10 @@ const watchclient = require("./lib/watchclient");
 require('dotenv').config();
 const got = require('got')
 const updatecmd = require("./lib/functions/updateCommandsDatabase")
-require("./lib/functions/rpsgamehandler")
+require("./lib/gamehandler/rpsgamehandler")
+require("./lib/gamehandler/emotegamehandler")
 require("./lib/functions/runningrpsgames")
+require("./lib/functions/runningEmoteGames")
 updatecmd();
 
 var emoteJSON={}
