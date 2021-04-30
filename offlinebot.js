67fd4fc066b4d2981405d5e4c64f88ff521a8d37
helltf <74050664+helltf@users.noreply.github.com>
2021-04-30 11:12:29 +0200

    added connect functions

--- offlinebot.js
+++ offlinebot.js
@@ -1,20 +1,29 @@
 const database = require('./database');
 database.connect();
 const requiredir = require("require-dir")
-require('./lib/client.js');
-require("./lib/watchclient");
+
 require('dotenv').config();
 requiredir("./lib/functions")
 requiredir("./lib/gamehandler")
+let helltfbot = require('./lib/client.js');
+let watchclient = require("./lib/watchclient");
 const livestatus = require("./lib/notify/updatelivestatus")
 const ATupdater = require("./lib/functions/ATHandler").updateAT
 const updatecmd = require("./lib/functions/updateCommandsDatabase")
+const addChannelsToWatchchannels=require("./lib/functions/updateWatchchannels")
 const start = async()=>{
     updatecmd();    
 
+    await addChannelsToWatchchannels()
+
+    helltfbot.connect()
+ 
+    watchclient.connect()
+ 
     await ATupdater()
 
    await livestatus.init()
+
 
     setInterval(livestatus.update,5000)
     
