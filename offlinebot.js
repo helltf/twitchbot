0567e5248d6246157d62b681b9bc66e741d78dae
helltf <74050664+helltf@users.noreply.github.com>
2021-05-08 17:12:27 +0200

    hb now global to avoid circular dependencies

--- offlinebot.js
+++ offlinebot.js
@@ -1,33 +1,35 @@
+#!/usr/bin/env node
+
 const database = require('./database');
 database.connect();
 const requiredir = require("require-dir")
-
 require('dotenv').config();
 requiredir("./lib/functions")
 requiredir("./lib/gamehandler")
-let helltfbot = require('./lib/client.js');
-let watchclient = require("./lib/watchclient");
+require("./lib/watchclient");
+const updateReadme=require("./lib/functions/updateReadMe")
 const livestatus = require("./lib/notify/updatelivestatus")
 const ATupdater = require("./lib/functions/ATHandler").updateAT
 const updatecmd = require("./lib/functions/updateCommandsDatabase")
-const addChannelsToWatchchannels=require("./lib/functions/updateWatchchannels")
+const addChannelsToWatchchannels=require("./lib/functions/updateWatchchannels");
+
+global.hb = {}
+hb.client=require('./lib/client.js').client
+hb.sendAllowedMessage=require('./lib/client.js').sendAllowedMessage
+hb.startClient=require("./lib/client").startClient
+hb.watchclient=require("./lib/watchclient").watchclient
+hb.watchclient.startwatchClient=require("./lib/watchclient").startwatchClient
+
 const start = async()=>{
-    updatecmd();    
-
+    await updatecmd();
+    await updateReadme();
     await addChannelsToWatchchannels()
-
-    helltfbot.connect()
- 
-    watchclient.connect()
- 
+    await hb.startClient()
+    await hb.watchclient.startwatchClient();
     await ATupdater()
-
-   await livestatus.init()
-
-
-    setInterval(livestatus.update,5000)
+    await livestatus.init()
     
+    setInterval(livestatus.update,5000)
     setInterval(ATupdater,3300000)
 }
-
 start();
