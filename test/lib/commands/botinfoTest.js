96fc8fb7de4ddc6b97f10f567fff638425172cfe
helltf <Dario.Zachow5530@gmail.com>
2021-05-19 21:04:33 +0200

    edited botinfo

--- test/lib/commands/botinfoTest.js
+++ test/lib/commands/botinfoTest.js
@@ -2,7 +2,7 @@ const botinfo = require("../../../lib/commands/botinfo").invocation
 const assert = require("chai").assert
 describe("commands",()=>{
     it("botinfo returns botinfo",async()=>{
-            let string = `This bot keeps track of your colorchanges in certain channels! If you want to use it just register your account with "hb register". After finishing you can register for colorchanges with "hb registercolor". Bot created by helltf. Git repository 👉 https://github.com/helltf/twitchbot/`
+            let string = `This bot keeps track of your colorchanges in certain channels! If you want to use it register for colorchanges with "hb registercolor". Bot created by helltf. Git repository 👉 https://github.com/helltf/twitchbot/`
             assert.equal(await botinfo() ,string)
      } )
     })
