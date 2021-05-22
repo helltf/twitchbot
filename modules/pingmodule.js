856a9cd8c9b0ad181142059bd73d0aea9cb0362c
helltf <Dario.Zachow5530@gmail.com>
2021-05-22 16:37:53 +0200

    fixed pingmodule

--- modules/pingmodule.js
+++ modules/pingmodule.js
@@ -5,11 +5,11 @@ hb.watchclient.on('chat',async(channel,userstate,message,self)=>{
     channel = channel.replace("#","")
     let pinguser = await database.getPingUser()
     for({username,user_id,regex} of pinguser){
-
+        if(username===userstate.username.toLowerCase()) return
         const matchRegex = new RegExp(regex,"gmi")
         let match = message.toLowerCase().match(matchRegex)
         if(match){
-            database.updateLastPing(user_id,channel,match[0],userstate.username.toLowerCase())
+            database.updateLastPing(user_id,channel,match[0].replace(/[\W]+/g),userstate.username.toLowerCase())
         }
     }
 })
