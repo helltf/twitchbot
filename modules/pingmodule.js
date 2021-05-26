9e768a36cba2e4846e44b1201de4b729b51e9511
helltf <Dario.Zachow5530@gmail.com>
2021-05-26 14:18:09 +0200

    fixed pingmodule

--- modules/pingmodule.js
+++ modules/pingmodule.js
@@ -1,18 +1,18 @@
 const database = require("./../database")
 const antiPingRegex = new RegExp("[\u034f\u2800(\u{e0000}\u{dc00})\u180e\ufeff\u2000-\u200d\u206D]","gu")
 
-hb.watchclient.on('chat',async(channel,{username},message,self)=>{
+hb.watchclient.on('chat',async(channel,userstate,message,self)=>{
     channel = channel.replace("#","")
     let pinguser = await database.getPingUser()
+    console.log(pinguser)
     for({username,user_id,regex} of pinguser){
-
-        if(username===username.toLowerCase()) continue
+        if(username===userstate.username.toLowerCase()) continue
 
         const matchRegex = new RegExp(regex,"gmi")
         let match = message.toLowerCase().replace(antiPingRegex,"").match(matchRegex)
 
         if(match){
-            database.updateLastPing(user_id,channel,match[0].replace(/[\W]+/g,""),username.toLowerCase())
+            database.updateLastPing(user_id,channel,match[0].replace(/[\W]+/g,""),userstate.username.toLowerCase())
         }
     }
 })
