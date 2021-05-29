f4690e4d834aa608cd719418f10e87cc1803d5b2
helltf <Dario.Zachow5530@gmail.com>
2021-05-29 13:09:09 +0200

    fixed module

--- modules/suggestionmodule.js
+++ modules/suggestionmodule.js
@@ -1,6 +1,8 @@
 hb.client.on('chat', (channel, {username}, message, self) => {
-	if(!(username==="helltf")||!(hb.suggestions.length===0)||(channel!="helltf")) return
+	if(!(username==="helltf")||(hb.suggestions.length===0)||(channel!="#helltf")) return
+	console.log(hb.suggestions)
 	let creator = hb.suggestions.map((suggestion)=>suggestion.user).join(", ")
-	message = `There are ${hb.suggestions.length} suggestions 👉  created by ${creator} AlienPls`
-	hb.sendAllowedMessage(channel,message)
+	message = `There is/are ${hb.suggestions.length} new suggestion/s 👉 created by ${creator} AlienPls`
+	hb.client.say(channel,`${message}`)
+	hb.suggestions=[]
 })
