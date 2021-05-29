107ba389d7282f7439152806fda5f88ec2d2e6a5
helltf <Dario.Zachow5530@gmail.com>
2021-05-29 14:44:20 +0200

    removed console.log and message

--- modules/suggestionmodule.js
+++ modules/suggestionmodule.js
@@ -1,8 +1,7 @@
 hb.client.on('chat', (channel, {username}, message, self) => {
 	if(!(username==="helltf")||(hb.suggestions.length===0)||(channel!="#helltf")) return
-	console.log(hb.suggestions)
 	let creator = hb.suggestions.map((suggestion)=>suggestion.user).join(", ")
-	message = `There is/are ${hb.suggestions.length} new suggestion/s 👉 created by ${creator} AlienPls`
+	message = `helltf, There is/are ${hb.suggestions.length} new suggestion/s 👉 created by ${creator} AlienPls`
 	hb.client.say(channel,`${message}`)
 	hb.suggestions=[]
 })
