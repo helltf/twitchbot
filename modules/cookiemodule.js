1c6c0435701738b9a0c2e8c885ee696f2ab11ea6
helltf <Dario.Zachow5530@gmail.com>
2021-06-02 13:52:04 +0200

    added logging

--- modules/cookiemodule.js
+++ modules/cookiemodule.js
@@ -4,11 +4,13 @@ hb.watchclient.on("action",(channel,user,message,self)=>{
     if(user["user-id"]!=425363834) return
     channel = channel.replace("#","")
     if(message.startsWith("[Cookies]")){
+        console.log(message)
         let username =  message.match(/(?<=]\s)[^[].+(?=\s->)/)[0]
         let amount = parseInt(message.match(/(?<=\().+(?=\))/)[0])
         database.addNewCookieEvent(username, amount, channel)
     }
     if(message.startsWith("[Shop]")){
+        console.log(message)
         let username =  message.match(/(?<=]\s).+(?=,)/)[0]
         database.addNewCookieReset(username, channel)
     }
