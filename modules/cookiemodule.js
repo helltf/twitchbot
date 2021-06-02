549b4ed35ea00d4c141cd8385fe149e2f0058411
helltf <Dario.Zachow5530@gmail.com>
2021-06-02 13:42:24 +0200

    fixed cookiemodule

--- modules/cookiemodule.js
+++ modules/cookiemodule.js
@@ -1,14 +1,15 @@
 const database = require("../database")
 
-hb.watchclient.on("chat",(channel, user, message, self)=>{
+hb.watchclient.on("action",(channel,user,message,self)=>{
     if(user["user-id"]!=425363834) return
+    channel = channel.replace("#","")
     if(message.startsWith("[Cookies]")){
         let username =  message.match(/(?<=]\s)[^[].+(?=\s->)/)[0]
         let amount = parseInt(message.match(/(?<=\().+(?=\))/)[0])
-        database.addNewCookieEvent(username, amount)
+        database.addNewCookieEvent(username, amount, channel)
     }
     if(message.startsWith("[Shop]")){
-        let username =  message.match(/(?<=]\s).+(?=,)/)
-        database.addNewCookieReset(username)
+        let username =  message.match(/(?<=]\s).+(?=,)/)[0]
+        database.addNewCookieReset(username, channel)
     }
 })
