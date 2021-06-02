ad8c3595895ce45dba9d79638ebcbc0b8e282d47
helltf <Dario.Zachow5530@gmail.com>
2021-06-02 09:21:15 +0200

    refactored code

--- offlinebot.js
+++ offlinebot.js
@@ -7,10 +7,10 @@ requiredir("./lib/functions")
 requiredir("./lib/gamehandler")
 require("./lib/watchclient");
 
-const updateReadme=require("./lib/functions/updateReadMe")
+const updateReadme = require("./lib/functions/updateReadMe")
 const livestatus = require("./lib/notify/updatelivestatus")
 const ATupdater = require("./lib/functions/ATHandler").updateAT
-const updatecmd = require("./lib/functions/updateCommandsDatabase");
+const updateCommands = require("./lib/functions/updateCommandsDatabase");
 const { initEmotes ,updateEmotes} = require('./lib/notify/updateemotes');
 
 global.games = {}
@@ -18,11 +18,11 @@ games.rps = []
 games.emote = []
 
 global.hb = {}
-hb.client=require('./lib/client.js').client
-hb.sendAllowedMessage=require('./lib/client.js').sendAllowedMessage
-hb.startClient=require("./lib/client").startClient
-hb.watchclient=require("./lib/watchclient").watchclient
-hb.watchclient.startwatchClient=require("./lib/watchclient").startwatchClient
+hb.client = require('./lib/client.js').client
+hb.sendAllowedMessage = require('./lib/client.js').sendAllowedMessage
+hb.startClient = require("./lib/client").startClient
+hb.watchclient = require("./lib/watchclient").watchclient
+hb.watchclient.startwatchClient = require("./lib/watchclient").startwatchClient
 hb.database = require("./database")
 hb.util = require("./lib/functions/functions")
 hb.suggestions = []
@@ -32,18 +32,23 @@ requiredir("./modules/")
 const start = async()=>{
     await database.connect(process.env.ENVIRONMENT);
     if(process.env.ENVIRONMENT==="test") return 
-    await updatecmd();
+    await updateCommands();
     await updateReadme();
     await hb.startClient()
     await hb.watchclient.startwatchClient();
     await ATupdater()
+    if(process.env.ENVIRONMENT === "dev") return
+
     startLiveUpdates()
-    await initEmotes()
-    setInterval( updateEmotes,5000)
+    startEmoteUpdates()
     setInterval(ATupdater,3300000)
 }
 start();
-const startLiveUpdates  =async()=>{
+const startLiveUpdates  =async ()=>{
     await livestatus.init()
     setInterval(livestatus.update,5000)
+}
+const startEmoteUpdates = async ()=>{
+    await initEmotes()
+    setInterval( updateEmotes,5000)
 }
